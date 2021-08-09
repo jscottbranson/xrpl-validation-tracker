943c7a3df3e1eac8556647bfb9c5213259112945
J. Scott Branson <the@rabbitkick.club>
2021-08-09 14:16:12 -0400

    Make vl.xrplf.org the default.

--- xrpl_validation_tracker/settings_supplemental.py
+++ xrpl_validation_tracker/settings_supplemental.py
@@ -10,7 +10,8 @@ SLEEP_CYCLE = 600 # Time (seconds) to sleep between runs
 
 HTTP_TIMEOUT = 20 # Time (seconds) to wait for HTTP responses
 
-DUNL_ADDRESS = "https://vl.ripple.com" # Address where the dUNL is served
+DUNL_ADDRESS = "https://vl.xrplf.org" # Address where the dUNL is served
+#DUNL_ADDRESS = "https://vl.ripple.com" # Address where the dUNL is served
 
 #List of URLs to query for manifests
 MANIFEST_QUERY_WS = [
